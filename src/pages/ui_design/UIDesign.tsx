8529b514ffa045302032cc2f6dc39e907e154c63
minhtiep <quangvantiepp@gmail.com>
2024-03-26 23:19:32 +0700

    implement render images

--- src/pages/ui_design/UIDesign.tsx
+++ src/pages/ui_design/UIDesign.tsx
@@ -1,46 +1,181 @@
-import React from "react";
-import im1 from "../../images/ui_design/p1/5aeacacab5841ada4395.jpg";
-import im2 from "../../images/ui_design/p1/5afe34d74b99e4c7bd88.jpg";
-import im3 from "../../images/ui_design/p1/6581a9add6e379bd20f2.jpg";
-import im4 from "../../images/ui_design/p1/9d2b8701f84f57110e5e.jpg";
-import im5 from "../../images/ui_design/p1/adf23cd5439becc5b58a.jpg";
-import im6 from "../../images/ui_design/p1/c0a7af85d0cb7f9526da.jpg";
-
-interface DataImages {
-  name: string;
-}
-
-const data: DataImages[] = [
-  {
-    name: im1,
-  },
-  {
-    name: im2,
-  },
-  {
-    name: im3,
-  },
-  {
-    name: im4,
-  },
-  {
-    name: im5,
-  },
-  {
-    name: im6,
-  },
-];
+import React, { useEffect, useState } from "react";
+import p1a from "../../images/ui_design/p1/5aeacacab5841ada4395.jpg";
+import { css } from "@emotion/css";
 
 const UIDesign = () => {
+  const [saveImages, setSaveImage] = useState<any>([]);
+  // create to object
+  // function importAll(r: __WebpackModuleApi.RequireContext) {
+  //   let images: { [key: string]: string } = {};
+  //   r.keys().map((item, index) => {
+  //     images[item.replace("./", "")] = r(item);
+  //   });
+  //   return images;
+  // }
+
+  // create to array
+  function importAll(r: any) {
+    return r.keys().map(r);
+  }
+
+  // Sử dụng require.context để import tất cả các file trong thư mục
+  // must install npm install @types/webpack-env --save-dev
+  let p1 = importAll(
+    require.context(`../../images/ui_design/p1/`, false, /\.(png|jpe?g|svg)$/)
+  );
+
+  let p2 = importAll(
+    require.context("../../images/ui_design/p2/", false, /\.(png|jpe?g|svg)$/)
+  );
+
+  let p3 = importAll(
+    require.context("../../images/ui_design/p3/", false, /\.(png|jpe?g|svg)$/)
+  );
+
+  let p4 = importAll(
+    require.context("../../images/ui_design/p4/", false, /\.(png|jpe?g|svg)$/)
+  );
+
+  let p5 = importAll(
+    require.context("../../images/ui_design/p5/", false, /\.(png|jpe?g|svg)$/)
+  );
+
+  let p6 = importAll(
+    require.context("../../images/ui_design/p6/", false, /\.(png|jpe?g|svg)$/)
+  );
+
+  let p7 = importAll(
+    require.context("../../images/ui_design/p7/", false, /\.(png|jpe?g|svg)$/)
+  );
+
+  let p8 = importAll(
+    require.context("../../images/ui_design/p8/", false, /\.(png|jpe?g|svg)$/)
+  );
+
+  let p9 = importAll(
+    require.context("../../images/ui_design/p9/", false, /\.(png|jpe?g|svg)$/)
+  );
+  let p10 = importAll(
+    require.context("../../images/ui_design/p10/", false, /\.(png|jpe?g|svg)$/)
+  );
+
+  let p11 = importAll(
+    require.context("../../images/ui_design/p11/", false, /\.(png|jpe?g|svg)$/)
+  );
+
+  let p12 = importAll(
+    require.context("../../images/ui_design/p12/", false, /\.(png|jpe?g|svg)$/)
+  );
+
+  let p13 = importAll(
+    require.context(`../../images/ui_design/p13/`, false, /\.(png|jpe?g|svg)$/)
+  );
+
+  let p14 = importAll(
+    require.context("../../images/ui_design/p14/", false, /\.(png|jpe?g|svg)$/)
+  );
+
+  let p15 = importAll(
+    require.context(`../../images/ui_design/p15/`, false, /\.(png|jpe?g|svg)$/)
+  );
+
+  let p16 = importAll(
+    require.context(`../../images/ui_design/p16/`, false, /\.(png|jpe?g|svg)$/)
+  );
+
+  let p17 = importAll(
+    require.context(`../../images/ui_design/p17/`, false, /\.(png|jpe?g|svg)$/)
+  );
+
+  let p18 = importAll(
+    require.context(`../../images/ui_design/p18/`, false, /\.(png|jpe?g|svg)$/)
+  );
+
+  let p19 = importAll(
+    require.context(`../../images/ui_design/p19/`, false, /\.(png|jpe?g|svg)$/)
+  );
+
+  let p20 = importAll(
+    require.context(`../../images/ui_design/p20/`, false, /\.(png|jpe?g|svg)$/)
+  );
+
+  let p21 = importAll(
+    require.context(`../../images/ui_design/p21/`, false, /\.(png|jpe?g|svg)$/)
+  );
+
+  let p22 = importAll(
+    require.context(`../../images/ui_design/p22/`, false, /\.(png|jpe?g|svg)$/)
+  );
+
+  let images = [
+    ...p1,
+    "step",
+    ...p2,
+    "step",
+    ...p3,
+    "step",
+    ...p4,
+    "step",
+    ...p5,
+    "step",
+    ...p6,
+    "step",
+    ...p7,
+    "step",
+    ...p8,
+    "step",
+    ...p9,
+    "step",
+    ...p10,
+    "step",
+    ...p11,
+    "step",
+    ...p12,
+    "step",
+    ...p13,
+    "step",
+    ...p14,
+    "step",
+    ...p15,
+    "step",
+    ...p16,
+    "step",
+    ...p17,
+    "step",
+    ...p18,
+    "step",
+    ...p19,
+    "step",
+    ...p20,
+    "step",
+    ...p21,
+    "step",
+    ...p22,
+    "step",
+  ];
+  console.log(saveImages);
+  let count = 0;
   return (
     <div>
-      {data?.map((item) => {
+      {images.map((item: any, index: number) => {
+        if (item === "step") {
+          return (
+            <div
+              className={css`
+                background-color: #41e441;
+                padding: 5px;
+              `}
+            >
+              Step {index - count}
+            </div>
+          );
+        }
+
+        count++;
         return (
-          <img
-            src={item.name}
-            alt=""
-            style={{ width: "40vw", height: "40%" }}
-          />
+          <>
+            <img src={item} alt="" style={{ width: "40vw", height: "40%" }} />
+          </>
         );
       })}
     </div>
